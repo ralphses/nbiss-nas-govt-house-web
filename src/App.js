37f9756c533d3a9c888c7670f941d508a553a719
Raphael Eze <eze.raph@gmail.com>
2024-01-04 17:26:53 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import GuestNavbar from "./components/NavBar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import AddCrimeSceneForm from "./pages/AddScene";
+import AddCrimeForm from "./pages/AddCrimeForm";
+import SafetyTips from "./pages/SafetyTips";
+import SuccessPage from "./pages/SuccessPage";
+import AllCrimes from "./pages/AllCrimes";
+import Analytics from "./pages/Analytics";
+import LineChart from "./pages/Analytics";
+import ThreatAlerts from "./pages/ThreatAlerts";
+import ProfileSettings from "./pages/ProfileSettings";
 
 function App() {
+  const isLoggedIn = () => {
+    return localStorage.getItem("authenticated") === "true";
+  };
+
+  // PrivateRoute component to handle redirection
+  const PrivateRoute = ({ element, ...props }) => {
+    return isLoggedIn() ? (
+      element
+    ) : (
+      <Navigate to="/login" state={{ from: props.location }} />
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <GuestNavbar />
+      <Routes>
+      <Route exact path="/" element={<Home />} />
+        <Route exact path="/crimes" element={<AllCrimes />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/success" element={<SuccessPage />} />
+        <Route exact path="/dashboard/analytics" element={<LineChart />} />
+        <Route exact path="/dashboard/threats" element={<ThreatAlerts />} />
+        <Route exact path="/dashboard/settings" element={<ProfileSettings />} />
+        <Route
+          path="/dashboard"
+          element={<PrivateRoute element={<Dashboard />} />}
+        />
+        <Route
+          path="/dashboard/add-crime"
+          element={<PrivateRoute element={<AddCrimeForm />} />}
+        />
+        <Route
+          exact
+          path="/add-crime-scene"
+          element={<AddCrimeSceneForm />}
+        />
+        <Route exact path="/safety-tips" element={<SafetyTips />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
